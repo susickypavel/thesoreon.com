b0983e9c68715b80582b0d84faf23648ea16c331
Pavel Susicky <pavels.sus@gmail.com>
2019-10-26 13:47:36 +0200

    test: 💍 add mocks for gatsby internals

--- src/__tests__/pages/index.test.tsx
+++ src/__tests__/pages/index.test.tsx
@@ -1,9 +1,20 @@
 import React from "react"
+import { useStaticQuery } from "gatsby"
 import { render } from "@testing-library/react"
 
 import Index from "../../pages"
 
 describe("Index page", () => {
+  beforeAll(() => {
+    ;(useStaticQuery as jest.Mock).mockReturnValue({
+      site: {
+        siteMetadata: {
+          description: "Test",
+          twitter: "Hello World",
+        },
+      },
+    })
+  })
   test("should render", () => {
     const { container } = render(<Index />)
 
