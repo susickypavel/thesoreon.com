aef0970c72cefbcaa69d09362893be693ff14293
Pavel Susicky <pavels.sus@gmail.com>
2019-10-27 18:28:18 +0100

    fix: 🐛 edit index page to pass tests in CI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,19 @@ import React from "react"
 
 import Layout from "~/components/Layout"
 
-const Index: React.FC = () => {
+interface Props {
+  location: Location
+}
+
+const Index: React.FC<Props> = ({ location }) => {
   return (
-    <Layout>
+    <Layout
+      title="Home"
+      metaData={{
+        pathname: location.pathname,
+        customType: "article",
+      }}
+    >
       <h1>Hello World</h1>
     </Layout>
   )
