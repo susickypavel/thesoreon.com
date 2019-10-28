54f85f8fa121013cf9db9612935ef0d8a9ae366e
Pavel Susicky <pavels.sus@gmail.com>
2019-10-28 11:41:35 +0100

    feat: 🎸 add functionality to programmatically create blog pages

---
+++ src/components/blog/post-page-template.tsx
@@ -0,0 +1,49 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+
+import { BlogPostPageQuery } from "~/graphqlTypes"
+
+import Layout from "../Layout"
+
+interface Props {
+  data: BlogPostPageQuery
+}
+
+const PageTemplate: React.FC<Props> = ({ data: { mdx } }) => {
+  const {
+    body,
+    frontmatter: { title, description },
+    fields: { slug },
+  } = mdx
+
+  return (
+    <Layout
+      title={title}
+      metaData={{
+        pathname: slug,
+        customDescription: description,
+        customType: "article",
+      }}
+    >
+      <h1>{title}</h1>
+      <MDXRenderer>{body}</MDXRenderer>
+    </Layout>
+  )
+}
+
+export default PageTemplate
+export const pageQuery = graphql`
+  query BlogPostPage($id: String) {
+    mdx(id: { eq: $id }) {
+      body
+      frontmatter {
+        title
+        description
+      }
+      fields {
+        slug
+      }
+    }
+  }
+`
