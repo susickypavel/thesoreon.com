40d3b873c9d16ae06fac43f3254df5c0518e249f
Pavel Susicky <pavels.sus@gmail.com>
2019-10-28 10:05:38 +0100

    refactor: 💡 change theme colors

--- src/css/global-styles.ts
+++ src/css/global-styles.ts
@@ -17,11 +17,21 @@ export default css`
   }
 
   body.light {
-    --primary-bg-color: white;
+    --primary-bg-color: lightblue;
     --primary-fg-color: black;
+
+    --secondary-bg-color: white;
+    --secondary-fg-color: black;
+
+    --links-color: #ef1957;
   }
   body.dark {
-    --primary-bg-color: black;
-    --primary-fg-color: white;
+    --primary-bg-color: #343434;
+    --primary-fg-color: #99b2cc;
+
+    --secondary-bg-color: #1a232a;
+    --secondary-fg-color: white;
+
+    --links-color: #ef1957;
   }
 `
