134e66dee89f29466466fb7410cfc8f9ecaa906b
Pavel Susicky <pavels.sus@gmail.com>
2019-10-28 22:05:55 +0100

    feat: 🎸 add custom components to MDX elements

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,32 @@
 import React from "react"
+import { MDXProvider } from "@mdx-js/react"
+
 import ThemeProvider from "./src/components/theme-provider/theme-provider"
+import {
+  Heading,
+  Paragraph,
+  Blockquote,
+  OrderedList,
+  UnorderedList,
+} from "./src/components/blog/md-components/index"
 
 export const wrapRootElement = ({ element }) => {
-  return <ThemeProvider>{element}</ThemeProvider>
+  return (
+    <MDXProvider
+      components={{
+        h1: props => <Heading {...props} heading="h1" />,
+        h2: props => <Heading {...props} heading="h2" />,
+        h3: props => <Heading {...props} heading="h3" />,
+        h4: props => <Heading {...props} heading="h4" />,
+        h5: props => <Heading {...props} heading="h5" />,
+        h6: props => <Heading {...props} heading="h6" />,
+        p: props => <Paragraph {...props} />,
+        blockquote: props => <Blockquote {...props} />,
+        ol: props => <OrderedList {...props} />,
+        ul: props => <UnorderedList {...props} />,
+      }}
+    >
+      <ThemeProvider>{element}</ThemeProvider>
+    </MDXProvider>
+  )
 }
