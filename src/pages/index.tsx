b4389ddbaae3c1cce24775c27c9277b6308caa08
Pavel Susicky <pavels.sus@gmail.com>
2019-10-28 12:57:46 +0100

    feat: 🎸 add blog post list component
    This component renders list of all blog posts available

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,45 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "~/components/Layout"
+import BlogPostList from "~/components/blog/blog-post-list"
+import { BlogPostsListQuery } from "~/graphqlTypes"
 
 interface Props {
   location: Location
+  data: BlogPostsListQuery
 }
 
-const Index: React.FC<Props> = ({ location }) => {
+const Index: React.FC<Props> = ({ location, data }) => {
   return (
     <Layout
       title="Home"
       metaData={{
         pathname: location.pathname,
-        customType: "article",
       }}
     >
-      <h1>Hello World</h1>
+      <h1>Blog posts</h1>
+      <BlogPostList data={data} />
     </Layout>
   )
 }
+
+export const PageQuery = graphql`
+  query BlogPostsList {
+    allMdx {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Index
