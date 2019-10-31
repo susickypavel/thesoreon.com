7beb95ad83336d7c25597a11ce9d0ad1d3a458c7
Pavel Susicky <pavels.sus@gmail.com>
2019-10-31 14:41:49 +0100

    fix: 🐛 add Theme and MDX providers to SSR html

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,13 @@
-import { createElement } from "react"
+import React, { createElement } from "react"
+import { MDXProvider } from "@mdx-js/react"
+import ThemeProvider from "./src/components/theme-provider/theme-provider"
+import {
+  Heading,
+  Paragraph,
+  Blockquote,
+  OrderedList,
+  UnorderedList,
+} from "./src/components/blog/md-components/index"
 
 const applyDarkModeClass = `
 (function() {
@@ -34,4 +43,25 @@ export const onRenderBody = ({ setPreBodyComponents }) => {
     },
   })
   setPreBodyComponents([script])
+}
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <MDXProvider
+      components={{
+        h1: props => <Heading {...props} heading="h1" />,
+        h2: props => <Heading {...props} heading="h2" />,
+        h3: props => <Heading {...props} heading="h3" />,
+        h4: props => <Heading {...props} heading="h4" />,
+        h5: props => <Heading {...props} heading="h5" />,
+        h6: props => <Heading {...props} heading="h6" />,
+        p: props => <Paragraph {...props} />,
+        blockquote: props => <Blockquote {...props} />,
+        ol: props => <OrderedList {...props} />,
+        ul: props => <UnorderedList {...props} />,
+      }}
+    >
+      <ThemeProvider>{element}</ThemeProvider>
+    </MDXProvider>
+  )
 }
