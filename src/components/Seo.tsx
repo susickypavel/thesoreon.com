bbe98d878bd1e182dc62681b242a8d85a2fc6ab7
Pavel Sušický <pavels.sus@gmail.com>
2019-11-16 16:01:32 +0100

    fix: 🐛 flash of theme color set through meta tag
    Closes #25

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -46,23 +46,26 @@ const Seo: React.FC<Props> = ({
   }, [])
 
   return (
-    <Head defer={false} titleTemplate="%s | Pavel Susicky" htmlAttributes={{ lang: "en" }}>
-      <title>{title}</title>
-      <meta name="description" content={customDescription || description} />
+    <>
+      <Head defer={false} titleTemplate="%s | Pavel Susicky" htmlAttributes={{ lang: "en" }}>
+        <title>{title}</title>
+        <meta name="description" content={customDescription || description} />
 
-      <meta property="og:url" content={`https://thesoreon.com${pathname}`} />
-      <meta property="og:type" content={customType} />
-      <meta property="og:title" content={composedTitle} />
-      <meta property="og:description" content={customDescription || description} />
-      <meta property="og:image" content={`https://thesoreon.com/${image}`} />
+        <meta property="og:url" content={`https://thesoreon.com${pathname}`} />
+        <meta property="og:type" content={customType} />
+        <meta property="og:title" content={composedTitle} />
+        <meta property="og:description" content={customDescription || description} />
+        <meta property="og:image" content={`https://thesoreon.com/${image}`} />
 
-      <meta name="twitter:image" content={`https://thesoreon.com/${image}`} />
-      <meta name="twitter:creator" content={twitter} />
-      <meta name="twitter:title" content={composedTitle} />
-      <meta name="twitter:description" content={customDescription || description} />
-
-      <meta name="theme-color" content={activeTheme === "light" ? "lightblue" : "#212121"} />
-    </Head>
+        <meta name="twitter:image" content={`https://thesoreon.com/${image}`} />
+        <meta name="twitter:creator" content={twitter} />
+        <meta name="twitter:title" content={composedTitle} />
+        <meta name="twitter:description" content={customDescription || description} />
+      </Head>
+      <Head>
+        <meta name="theme-color" content={activeTheme === "light" ? "lightblue" : "#212121"} />
+      </Head>
+    </>
   )
 }
 
