9536af9686df687d23cf6bab374a386a6e864bbd
Pavel Sušický <pavels.sus@gmail.com>
2019-11-29 16:51:53 +0100

    ci: 🎡 edit github actions workflows

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,3 @@
-import "@testing-library/cypress/add-commands"
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
