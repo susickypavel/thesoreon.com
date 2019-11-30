b65ef28cec887a473af27d9e3da475270fa35a87
Pavel Sušický <pavels.sus@gmail.com>
2019-11-30 19:32:21 +0100

    feat: 🎸 add theme toggle button

---
+++ src/__tests__/components/theme-toggle.test.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import { render, fireEvent } from "@testing-library/react"
+import { ThemeToggle } from "~/components/theme-toggle"
+
+interface ExtendGlobal extends NodeJS.Global {
+  __theme: string
+  __setPreferredTheme: (theme: string) => void
+}
+
+const _global: ExtendGlobal = global as any
+
+describe("Theme toggle", () => {
+  const renderComponent = () => {
+    return render(<ThemeToggle />)
+  }
+
+  beforeEach(() => {
+    _global.__theme = "dark"
+    _global.__setPreferredTheme = function(theme: string) {
+      _global.__theme = theme
+    }
+  })
+
+  it("should have dark theme by default", () => {
+    expect(_global.__theme).toBe("dark")
+  })
+
+  it("should switch to light theme when button is clicked", () => {
+    const { getByLabelText } = renderComponent()
+
+    const button = getByLabelText("Switch theme")
+    fireEvent.click(button)
+
+    expect(_global.__theme).toBe("light")
+  })
+})
