1ebdd27eabd9956f06054eed70a6c8c2217c81ba
Pavel Sušický <pavels.sus@gmail.com>
2019-11-30 21:18:52 +0100

    feat: 🎸 add icon to Theme Toggle indicating current theme

--- src/components/actions-link-bar/theme-toggle/index.tsx
+++ src/components/actions-link-bar/theme-toggle/index.tsx
@@ -1,24 +1,38 @@
 import React, { useState, useEffect } from "react"
+import { FaSun, FaMoon } from "react-icons/fa"
+import { IconType } from "react-icons/lib/cjs"
+
 import { ThemeToggleButton } from "./styles"
 
+const createIcon = ({ icon, color }: State) => React.createElement(icon, { size: "3rem", color })
+
+interface State {
+  icon: IconType
+  color: string
+  theme: "dark" | "light"
+}
+
+const lightThemeValues: State = { theme: "light", icon: FaSun, color: "yellow" }
+const darkThemeValues: State = { theme: "dark", icon: FaMoon, color: "white" }
+
 export const ThemeToggle: React.FC = () => {
-  const [activeTheme, setActiveTheme] = useState("light")
+  const [activeTheme, setActiveTheme] = useState<State>(lightThemeValues)
 
   const handleClick = () => {
     setActiveTheme(() => {
-      const themeToToggle = activeTheme === "light" ? "dark" : "light"
-      window.__setPreferredTheme(themeToToggle)
-      return themeToToggle
+      const toggledValues: State = activeTheme.theme === "dark" ? lightThemeValues : darkThemeValues
+      window.__setPreferredTheme(toggledValues.theme)
+      return toggledValues
     })
   }
 
   useEffect(() => {
-    setActiveTheme(window.__theme === "dark" ? "dark" : "light")
+    setActiveTheme(window.__theme === "dark" ? darkThemeValues : lightThemeValues)
   }, [])
 
   return (
     <ThemeToggleButton aria-label="Switch theme" onClick={handleClick}>
-      Toggle
+      {createIcon(activeTheme.theme === "dark" ? darkThemeValues : lightThemeValues)}
     </ThemeToggleButton>
   )
 }
