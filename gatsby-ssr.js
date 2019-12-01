69ad5f3636699eb245399a76653fa6f7fe57f710
Pavel Sušický <pavels.sus@gmail.com>
2019-12-01 11:28:45 +0100

    feat: 🎸 add dark/light theme provider

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,13 @@
 import { createElement } from "react"
 import { PageLayout } from "./src/components/PageLayout"
+import { ThemeProvider } from "./src/components/providers/theme"
 
 export const wrapPageElement = ({ element, props }) => {
   return <PageLayout {...props}>{element}</PageLayout>
+}
+
+export const wrapRootElement = ({ element }) => {
+  return <ThemeProvider>{element}</ThemeProvider>
 }
 
 const applyDarkModeClass = `
