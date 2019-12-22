e6bb25992247470adc72856540ed11b22e8169bd
Pavel Sušický <pavels.sus@gmail.com>
2019-12-22 13:53:37 +0100

    wipe theme

--- src/css/global-styles.ts
+++ src/css/global-styles.ts
@@ -1,5 +1,4 @@
 import { css } from "@emotion/core"
-import { darken, lighten } from "polished"
 
 import { BODY_MINWIDTH } from "./constants"
 
@@ -32,46 +31,6 @@ const globalStyles = css`
   #___gatsby,
   #gatsby-focus-wrapper {
     height: 100%;
-  }
-
-  body.dark {
-    background: #1b1b1b;
-    color: white;
-
-    transition: all 0.2s linear;
-
-    --secondary-bg-color: ${lighten(0.1, "#1b1b1b")};
-
-    --action-buttons-default: #03a9f4;
-    --action-buttons-focus: ${lighten(0.25, "#03a9f4")};
-    --action-buttons-icon: #212121;
-
-    --blog-preview-boxshadow: 0px 0px 4px rgba(255, 255, 255, 0.8);
-
-    --links-default: lightblue;
-    --links-focus: ${darken(0.25, "lightblue")};
-    --links-visited: ${lighten(0.25, "blue")};
-    --links-text: black;
-  }
-
-  body.light {
-    background: white;
-    color: black;
-
-    transition: all 0.2s linear;
-
-    --secondary-bg-color: ${darken(0.1, "white")};
-
-    --action-buttons-default: #90caf9;
-    --action-buttons-focus: ${darken(0.25, "#90caf9")};
-    --action-buttons-icon: #212121;
-
-    --blog-preview-boxshadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
-
-    --links-default: #f33b5d;
-    --links-focus: ${lighten(0.25, "#f33b5d")};
-    --links-visited: darkblue;
-    --links-text: black;
   }
 `
 
