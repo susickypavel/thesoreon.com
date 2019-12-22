824a634b02bf29802fc3e32a26c5a77f76dafad2
Pavel Sušický <pavels.sus@gmail.com>
2019-12-22 15:37:27 +0100

    feat: change typography of site to Consolas

--- src/utils/typography.tsx
+++ src/utils/typography.tsx
@@ -1,15 +1,9 @@
 import Typography from "typography"
 
 const typography = new Typography({
-  googleFonts: [
-    {
-      name: "IBM Plex Sans",
-      styles: ["400", "500", "600", "700&display=swap"],
-    },
-  ],
   baseFontSize: "20px",
   baseLineHeight: 1.666,
-  headerFontFamily: ["IBM Plex Sans"],
-  bodyFontFamily: ["IBM Plex Sans"],
+  headerFontFamily: ["Consolas"],
+  bodyFontFamily: ["Consolas"],
 })
 export default typography
