2a4d4f4d57a6a28fc071be8bacd21216d2b29deb
Pavel Sušický <pavels.sus@gmail.com>
2019-12-29 12:19:18 +0100

    chore: prettier and eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React from "react"
 
-import styled from "@emotion/styled";
+import styled from "@emotion/styled"
 
-const Child = styled.h1``;
+const Child = styled.h1``
 
 const Parent = styled.div`
   background-color: red;
   ${Child} {
     color: lime;
   }
-`;
+`
 
 const Index: React.FC = () => {
   return (
     <Parent>
       <Child>Hello World</Child>
     </Parent>
-  );
-};
+  )
+}
 
-export default Index;
+export default Index
