9f0d9be34f17b22c7cd0c7744640a736b46dda9a
Pavel Sušický <pavels.sus@gmail.com>
2019-12-29 12:57:01 +0100

    feat: css globals

--- src/components/page-layout/page-layout.component.tsx
+++ src/components/page-layout/page-layout.component.tsx
@@ -1,5 +1,14 @@
 import React from "react"
+import { Global } from "@emotion/core"
+
+import resetStyles from "~/css/reset-styles"
+import globalStyles from "~/css/global-styles"
 
 export const PageLayout: React.FC = ({ children }) => {
-  return <div>{children}</div>
+  return (
+    <>
+      <Global styles={[resetStyles, globalStyles]} />
+      {children}
+    </>
+  )
 }
