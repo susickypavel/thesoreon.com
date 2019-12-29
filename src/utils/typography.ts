110748e2cfe0a7e183e59730db1c6f9a5b683842
Pavel Sušický <pavels.sus@gmail.com>
2019-12-29 13:22:37 +0100

    feat: typography.js

---
+++ src/utils/typography.ts
@@ -0,0 +1,9 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "20px",
+  baseLineHeight: 1.666,
+  headerFontFamily: ["Consolas"],
+  bodyFontFamily: ["Consolas"],
+})
+export default typography
