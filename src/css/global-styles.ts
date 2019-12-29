822df3d5d7ece222933d0a84ff5f579ac0a8f6f8
Pavel Sušický <pavels.sus@gmail.com>
2019-12-29 14:06:52 +0100

    feat: global styles

--- src/css/global-styles.ts
+++ src/css/global-styles.ts
@@ -1,5 +1,31 @@
 import { css } from "@emotion/core"
 
-const globalStyles = css``
+const globalStyles = css`
+  * {
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-size: 62.5%;
+    overflow-y: unset; /* This hides scrollbar caused by typography.js */
+  }
+
+  @media (max-width: 768px) {
+    :root {
+      font-size: 50%;
+    }
+  }
+
+  body {
+    min-width: 320px;
+  }
+
+  html,
+  body,
+  #___gatsby,
+  #gatsby-focus-wrapper {
+    height: 100%;
+  }
+`
 
 export default globalStyles
