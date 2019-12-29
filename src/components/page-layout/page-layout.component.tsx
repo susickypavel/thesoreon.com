46ad826a88a43d536367008c98107574ec7952a0
Pavel Sušický <pavels.sus@gmail.com>
2019-12-29 14:03:13 +0100

    feat: webgl site background

--- src/components/page-layout/page-layout.component.tsx
+++ src/components/page-layout/page-layout.component.tsx
@@ -4,9 +4,16 @@ import { Global } from "@emotion/core"
 import resetStyles from "~/css/reset-styles"
 import globalStyles from "~/css/global-styles"
 
+import MyLoadable from "../hoc/canvas-loadable"
+
+const SiteBackground = MyLoadable({
+  loader: () => import("../site-background/site-background-component"),
+})
+
 export const PageLayout: React.FC = ({ children }) => {
   return (
     <>
+      <SiteBackground />
       <Global styles={[resetStyles, globalStyles]} />
       {children}
     </>
