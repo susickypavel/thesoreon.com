802379efb0eecce0cd2731957f4f269aff4970a7
Pavel Sušický <pavels.sus@gmail.com>
2019-12-29 13:22:37 +0100

    feat: typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,8 @@ module.exports = {
       options: {
         isTSX: false, // @default value
         jsxPragma: `React`, // @default value
-        allExtensions: false // @default value
-      }
+        allExtensions: false, // @default value
+      },
     },
     {
       resolve: `gatsby-plugin-emotion`,
@@ -14,8 +14,14 @@ module.exports = {
         sourceMap: true, // @default value
         autoLabel: process.env.NODE_ENV !== "production", // @default value
         labelFormat: "[local]", // @default value
-        cssPropOptimization: true // @default value
-      }
-    }
-  ]
-};
+        cssPropOptimization: true, // @default value
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.ts`,
+      },
+    },
+  ],
+}
