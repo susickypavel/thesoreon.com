93be125198a1c095e20ddd8f92106092fbb2192a
Pavel Sušický <pavels.sus@gmail.com>
2019-12-30 13:50:11 +0100

    feat: qrcode in a blog post card

--- src/components/blog-post-card/blog-post-card.styles.ts
+++ src/components/blog-post-card/blog-post-card.styles.ts
@@ -5,20 +5,19 @@ import { animated } from "react-spring"
 export const BlogPostCardHeader = styled.h2`
   background: black;
   color: #02d8f3;
-  transform: scale(1.1);
   font-size: 2.4rem;
   padding: 0.4em;
 `
 
 export const BlogPostCardWrapper = styled(animated.div)`
-  background-color: #f8ee03;
-  max-width: 500px;
+  background-color: gray;
+  max-width: 50rem;
   width: 100%;
-  height: 250px;
-  box-shadow: 2px 2px 50px rgba(0, 0, 0, 0.2);
-  margin: 32px;
+  box-shadow: 2px 2px 16px rgba(0, 0, 0, 0.5);
+  margin-bottom: 32px;
   transform-style: preserve-3d;
   display: flex;
+
   position: relative;
 
   @media (max-width: 650px) {
@@ -34,6 +33,20 @@ export const BlogPostCardBody = styled.div``
 
 export const BlogPostCardDescription = styled.p`
   font-size: 1.6rem;
-  transform: translateZ(10px);
-  padding: 1.6em;
+  padding: 0.8em;
+`
+
+export const BlogPostCardInformationBar = styled.div`
+  font-size: 1.8rem;
+  font-weight: bold;
+  padding: 0.8em;
+  padding-bottom: 0;
+`
+
+export const BlogPostCardInformation = styled.span`
+  margin-right: 0.8em;
+
+  & svg {
+    vertical-align: top;
+  }
 `
