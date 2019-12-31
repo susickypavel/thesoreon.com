9a717cee50fde60516c5ef8280e0075295e825df
Pavel Sušický <pavels.sus@gmail.com>
2019-12-31 11:49:23 +0100

    feat: new blog post card styles

--- src/components/blog-post-list/blog-post-list.styles.ts
+++ src/components/blog-post-list/blog-post-list.styles.ts
@@ -3,6 +3,6 @@ import styled from "@emotion/styled"
 export const BlogPostListHolder = styled.div`
   display: flex;
   flex-flow: column wrap;
-  padding: 1.6em;
+  padding: 3.2em;
   align-items: center;
 `
