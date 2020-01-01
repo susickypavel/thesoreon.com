fc51cd12cacfde2b6b7d32e592b066b827ecc534
Pavel Sušický <pavels.sus@gmail.com>
2020-01-02 00:01:14 +0100

    feat: prismjs syntax highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { PageLayout } from "./src/components/page-layout/page-layout.component"
 
+import "prismjs/themes/prism-okaidia.css"
+
 export const wrapPageElement = ({ element, props }) => {
   return <PageLayout {...props}>{element}</PageLayout>
 }
