3e566e2af89680fab402fcb48d3b7e41deaf8a5e
Pavel Sušický <pavels.sus@gmail.com>
2020-01-05 10:05:20 +0100

    feat: reposition table of contents

--- src/templates/blog-post-page/table-of-contents/table-of-contents.styled.ts
+++ src/templates/blog-post-page/table-of-contents/table-of-contents.styled.ts
@@ -1,18 +1,24 @@
 import styled from "@emotion/styled"
 import { Link } from "gatsby"
 
-export const TableOfContentsHolder = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
+export const TableOfContentsHolder = styled.div<{ sidebar: boolean }>`
   background: black;
   font-size: 2.4rem;
-  width: 300px;
+  width: 30rem;
   line-height: 1.2;
   padding: 0.8em;
+  position: fixed;
+  transform: translateX(-35rem);
+  top: 16px;
+  z-index: 10;
+
+  @media (max-width: 98rem) {
+    display: none;
+  }
 `
 export const TableOfContentsLine = styled(Link)<{ selected: boolean }>(({ selected }) => ({
   color: selected ? "red" : "white",
+  fontWeight: "bold",
   textOverflow: "ellipsis",
   overflow: "hidden",
   whiteSpace: "nowrap",
