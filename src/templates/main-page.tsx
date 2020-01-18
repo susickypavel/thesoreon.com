fcbb03bda34dbb8ca95d25d7441fd3f35bc95614
Pavel Sušický <pavels.sus@gmail.com>
2020-01-18 17:53:18 +0100

    feat: desktop

--- src/templates/main-page.tsx
+++ src/templates/main-page.tsx
@@ -1,9 +1,21 @@
 import React from "react"
 
 import { BlogPostList } from "~/components/blog-post-list/blog-post-list.component"
+import { LandingPage } from "~/components/landing-page/landing-page.component"
+import Seo from "~/components/Seo/Seo.component"
 
-const MainPage: React.FC = () => {
-  return <BlogPostList />
+interface Props {
+  location: Location
+}
+
+const MainPage: React.FC<Props> = ({ location: { pathname } }) => {
+  return (
+    <div>
+      {/* <BlogPostList /> */}
+      <Seo customMetadata={{ pathname }} />
+      <LandingPage />
+    </div>
+  )
 }
 
 export default MainPage
