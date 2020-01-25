9e5c98e59e6cc386fbb3c9e31f0b2d94a42acedc
Pavel Sušický <pavels.sus@gmail.com>
2020-01-25 15:07:44 +0100

    feat: remove desktop

--- src/components/blog-post-list/blog-post-list.styles.ts
+++ src/components/blog-post-list/blog-post-list.styles.ts
@@ -2,7 +2,7 @@ import styled from "@emotion/styled"
 
 export const BlogPostListHolder = styled.div`
   display: flex;
-  flex-flow: column wrap;
+  flex-flow: row wrap;
   padding: 3.2em;
   align-items: center;
 `
