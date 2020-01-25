357ba797b00ee48226ec8f05607e5027cea9588e
Pavel Sušický <pavels.sus@gmail.com>
2020-01-25 22:02:16 +0100

    fix: remove old landing page component

--- src/templates/main-page.tsx
+++ src/templates/main-page.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 
 import { BlogPostList } from "~/components/blog-post-list/blog-post-list.component"
-import { LandingPage } from "~/components/landing-page/landing-page.component"
 import Seo from "~/components/Seo/Seo.component"
 
 interface Props {
@@ -13,7 +12,6 @@ const MainPage: React.FC<Props> = ({ location: { pathname } }) => {
     <div>
       {/* <BlogPostList /> */}
       <Seo customMetadata={{ pathname }} />
-      <LandingPage />
     </div>
   )
 }
