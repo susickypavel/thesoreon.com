c9965fccae2db05efa4b29f322e6bbeac0065586
Pavel Sušický <pavels.sus@gmail.com>
2020-02-01 23:32:12 +0100

    fix: tweaked few styles

--- src/components/blog-post-list/blog-post-list.styles.ts
+++ src/components/blog-post-list/blog-post-list.styles.ts
@@ -2,7 +2,7 @@ import styled from "@emotion/styled"
 
 export const BlogPostListHolder = styled.div`
   display: flex;
-  flex-flow: row wrap;
+  flex-flow: column wrap;
   padding: 3.2em;
   align-items: center;
 `
