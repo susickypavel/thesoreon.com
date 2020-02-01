7baa66c0cf0bd337bfb92a59d989c93bbdd44f9e
Pavel Sušický <pavels.sus@gmail.com>
2020-02-01 23:14:51 +0100

    feat: footer

--- src/components/page-layout/page-layout.component.tsx
+++ src/components/page-layout/page-layout.component.tsx
@@ -5,6 +5,7 @@ import resetStyles from "~/css/reset-styles"
 import globalStyles from "~/css/global-styles"
 import { MenuIconToggle } from "./menu-icon-toggle/menu-icon-toggle.component"
 import { Menu } from "./menu/menu.component"
+import { Footer } from "./footer/footer.component"
 
 export const PageLayout: React.FC = ({ children }) => {
   const [toggled, setToggled] = useState(false)
@@ -15,6 +16,7 @@ export const PageLayout: React.FC = ({ children }) => {
       <Menu toggled={toggled} />
       <MenuIconToggle setToggled={setToggled} />
       {children}
+      <Footer />
     </>
   )
 }
